9269167e833eaf7d284adf3db96268ee345cae0f
Julie Johnston <julie.johnston@gmail.com>
2022-12-28 15:17:23 -0800

    add trigger to dictionary

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ intents.messages = True
 client = commands.Bot(command_prefix="!",intents=intents)
 inter_client = InteractionClient(client)
 
-mom_words = ["ur mom", "ur mum", "your mom", "yer mum", "yer mom", "urmom", "urmum"]
+mom_words = ["ur mom", "ur mum", "your mom", "yer mum", "yer mom", "urmom", "urmum", "joe mama"]
 
 mom_feels = [
   "Ur mom just wants to spend time with you",
