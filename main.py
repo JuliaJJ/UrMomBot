ed58ea091e26ba0178be794e2a719662f2e131e3
Julie Johnston <julie.johnston@gmail.com>
2023-01-01 15:13:35 -0800

    testing threaded replies v2

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ async def gift(inter: ContextMenuInteraction):
 @inter_client.message_command(name="Reply With a Hug")
 async def hug_reply(inter: ContextMenuInteraction, message: discord.Message):
     if inter.message.content:
-      await message.reply(
+      await inter.message.reply(
           f"Ur Mom gave <@{inter.message.author.id}> a big hug."
       )
       await inter.respond("Hug has been sent!")
